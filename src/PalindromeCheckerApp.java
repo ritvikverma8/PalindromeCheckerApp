80404023a937052e1a27697671193afabb17b459
ritvikverma8 <ritvikverma826@gmail.com>
2026-02-23 11:08:42 +0530

    UC9: Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,56 +2,16 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
+    static boolean checkPalindrome(String org, int left, int right) {
+        if (left >= right) {
+            return true;
         }
-    }
 
-    static Node buildList(String org) {
-        Node head = null;
-        Node tail = null;
-
-        for (int i = 0; i < org.length(); i++) {
-            Node newNode = new Node(org.charAt(i));
-
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail      = newNode;
-            }
+        if (org.charAt(left) != org.charAt(right)) {
+            return false;
         }
-        return head;
-    }
 
-    static Node findMiddle(Node head) {
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    static Node reverseList(Node head) {
-        Node prev    = null;
-        Node current = head;
-
-        while (current != null) {
-            Node nextNode  = current.next;
-            current.next   = prev;
-            prev           = current;
-            current        = nextNode;
-        }
-        return prev;
+        return checkPalindrome(org, left + 1, right - 1);
     }
 
     public static void main(String[] args) {
@@ -61,25 +21,8 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String org = scan.nextLine().toLowerCase();
 
-        Node head = buildList(org);
 
-        Node middle      = findMiddle(head);
-        Node secondHalf  = reverseList(middle);
-        Node secondCopy  = secondHalf;
-
-
-        Node first  = head;
-        Node second = secondHalf;
-
-        boolean isPalindrome = true;
-
-        while (second != null) {
-            if (first.data != second.data) {
-                isPalindrome = false;
-            }
-            first  = first.next;
-            second = second.next;
-        }
+        boolean isPalindrome = checkPalindrome(org, 0, org.length() - 1);
 
         System.out.println();
 
