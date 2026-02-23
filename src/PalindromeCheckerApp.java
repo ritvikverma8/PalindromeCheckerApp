a608fe5cf94ce9ae3b43d115398cbdd9b8dd65f4
ritvikverma8 <ritvikverma826@gmail.com>
2026-02-23 11:02:52 +0530

    UC8: Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,34 +1,84 @@
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args){
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node buildList(String org) {
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < org.length(); i++) {
+            Node newNode = new Node(org.charAt(i));
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail      = newNode;
+            }
+        }
+        return head;
+    }
+
+    static Node findMiddle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    static Node reverseList(Node head) {
+        Node prev    = null;
+        Node current = head;
+
+        while (current != null) {
+            Node nextNode  = current.next;
+            current.next   = prev;
+            prev           = current;
+            current        = nextNode;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String org = scan.nextLine().toLowerCase();
 
-        Deque<Character> deque = new ArrayDeque<>();
+        Node head = buildList(org);
 
-        for (int i = 0; i < org.length(); i++) {
-            deque.addLast(org.charAt(i));
-        }
+        Node middle      = findMiddle(head);
+        Node secondHalf  = reverseList(middle);
+        Node secondCopy  = secondHalf;
+
+
+        Node first  = head;
+        Node second = secondHalf;
 
         boolean isPalindrome = true;
 
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear  = deque.removeLast();
-
-            if (front != rear) {
+        while (second != null) {
+            if (first.data != second.data) {
                 isPalindrome = false;
             }
+            first  = first.next;
+            second = second.next;
         }
 
         System.out.println();
