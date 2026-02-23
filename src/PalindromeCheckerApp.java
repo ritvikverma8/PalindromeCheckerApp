e8aa974b88eea2a4362812a4a0155ef8a5770c44
ritvikverma8 <ritvikverma826@gmail.com>
2026-02-23 09:43:17 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.00");
-        System.out.println("System initialized succesfully.");
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        int k=1;
+
+        for(int i =0 ; i < input.length()/2 ; i++){
+            if(input.charAt(i) != input.charAt((input.length())-i-1)){
+                k=-1;
+                break;
+            }else{
+                k=1;
+            }
+        }
+        System.out.println("Input Text : "+input);
+        if(k==1){
+            System.out.println("Is it a palindrome ? : true ");
+        }else{
+            System.out.println("Is it a palindrome ? : false ");
+        }
+
     }
 
 }
